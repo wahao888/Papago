e0e3483303aefdb0d4d67f336649c7d676179ce3
unknown <tinghaowang645@gmail.com>
2023-10-02 09:38:04 +0800

    update

--- papago/settings.py
+++ papago/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / "static"] #為了讓網頁讀取到static資料夾
 
 
