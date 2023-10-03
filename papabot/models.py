cd36313024f6e6b7a33ad32ee92fcaa846bdb0d3
Your Name <you@example.com>
2023-10-03 16:18:42 +0800

    linebot account link

--- papabot/models.py
+++ papabot/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class LineId(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='line')
+    line_id = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "lineId"
