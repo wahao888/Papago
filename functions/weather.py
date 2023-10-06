694fdb2e6fc5c2f07524b59ea6c9baa01e7a14b9
Your Name <you@example.com>
2023-10-06 16:58:15 +0800

    modify something

--- functions/weather.py
+++ functions/weather.py
@@ -1,8 +1,6 @@
 import requests
 from urllib.request import quote
 from datetime import datetime
-import requests
-
 
 
 def weather_search(city):
@@ -18,21 +16,21 @@ def weather_search(city):
         data = res.json()
         location = data["records"]["location"][0]["locationName"]
         weather_elements = data["records"]["location"][0]["weatherElement"]
-        msg = f"［{city}天氣預報］\n"
+        msg = f"【{city}天氣預報】\n"
         for i in range(3):
             st = datetime.strptime(
                 weather_elements[0]["time"][i]["startTime"], r"%Y-%m-%d %H:%M:%S")
-            start_time = st.strftime(r"%d號 %H點")
+            start_time = st.strftime(r"%m/%d %H:00")
             et = datetime.strptime(
                 weather_elements[0]["time"][i]["endTime"], r"%Y-%m-%d %H:%M:%S")
-            end_time = et.strftime(r"%d號 %H點")
+            end_time = et.strftime(r"%m/%d %H:00")
             weather_state = weather_elements[0]["time"][i]["parameter"]["parameterName"]
             rain_prob = weather_elements[1]["time"][i]["parameter"]["parameterName"]
             min_tem = weather_elements[2]["time"][i]["parameter"]["parameterName"]
             comfort = weather_elements[3]["time"][i]["parameter"]["parameterName"]
             max_tem = weather_elements[4]["time"][i]["parameter"]["parameterName"]
 
-            msg += f"{start_time}至{end_time}\n天氣為{weather_state}，{comfort}，最高溫{max_tem}度，最低溫{min_tem}度，降雨機率{rain_prob}%\n\n"
+            msg += f"{start_time} - {end_time}\n天氣為{weather_state}，{comfort}，最高溫{max_tem}度，最低溫{min_tem}度，降雨機率{rain_prob}%\n\n"
 
         return msg.strip()
     except:
